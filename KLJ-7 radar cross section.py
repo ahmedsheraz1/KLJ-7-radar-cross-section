7cdd8f52a48f781e7c5607c8acda7efb38ad53bc
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 19:07:38 +0500

    Create KLJ-7 radar cross section.py

---
+++ KLJ-7 radar cross section.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+17 /3
+5.666666666666667
+17 //3
+5
