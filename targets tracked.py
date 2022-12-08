31bfe953c0429378f3a01138753a6854a6d35111
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-08 09:13:24 +0500

    Create targets tracked.py

---
+++ targets tracked.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+import math
+math.cos(math.pi / 40)
+0.996917333733128
+math.log(1024, 2)
+10.0
